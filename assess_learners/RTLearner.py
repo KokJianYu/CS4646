7966bcfe44fa078695c06b231ab5f2f41e00b3b1
Kok Jian Yu <kokjianyu@gmail.com>
2019-09-16 12:46:28 -0400

    Add project 3

---
+++ assess_learners/RTLearner.py
@@ -0,0 +1,31 @@
+import numpy as np  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+class RTLearner(object):  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    def __init__(self, verbose = False):  		   	  			  	 		  		  		    	 		 		   		 		  
+        pass # move along, these aren't the drones you're looking for  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    def author(self):  		   	  			  	 		  		  		    	 		 		   		 		  
+        return 'jkok7' # replace tb34 with your Georgia Tech username  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    def addEvidence(self,dataX,dataY):  		   	  			  	 		  		  		    	 		 		   		 		  
+        """  		   	  			  	 		  		  		    	 		 		   		 		  
+        @summary: Add training data to learner  		   	  			  	 		  		  		    	 		 		   		 		  
+        @param dataX: X values of data to add  		   	  			  	 		  		  		    	 		 		   		 		  
+        @param dataY: the Y training values  		   	  			  	 		  		  		    	 		 		   		 		  
+        """  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+        # TODO		   	  			  	 		  		  		    	 		 		   		 		  
+        return None
+  		   	  			  	 		  		  		    	 		 		   		 		  
+    def query(self,points):  		   	  			  	 		  		  		    	 		 		   		 		  
+        """  		   	  			  	 		  		  		    	 		 		   		 		  
+        @summary: Estimate a set of test points given the model we built.  		   	  			  	 		  		  		    	 		 		   		 		  
+        @param points: should be a numpy array with each row corresponding to a specific query.  		   	  			  	 		  		  		    	 		 		   		 		  
+        @returns the estimated values according to the saved model.  		   	  			  	 		  		  		    	 		 		   		 		  
+        """  		   	  			  	 		  		  		    	 		 		   		 		  
+        # TODO
+        return None
+  		   	  			  	 		  		  		    	 		 		   		 		  
+if __name__=="__main__":  		   	  			  	 		  		  		    	 		 		   		 		  
+    print("the secret clue is 'zzyzx'")  		   	  			  	 		  		  		    	 		 		   		 		  
