ce9573a5ceb6f89c3900d1fa96456b40cfdbe0d4
Kok Jian Yu <kokjianyu@gmail.com>
2019-10-05 09:09:27 -0400

    project 4

---
+++ defeat_learners/gen_data.py
@@ -0,0 +1,53 @@
+"""  		   	  			  	 		  		  		    	 		 		   		 		  
+template for generating data to fool learners (c) 2016 Tucker Balch  		   	  			  	 		  		  		    	 		 		   		 		  
+Copyright 2018, Georgia Institute of Technology (Georgia Tech)  		   	  			  	 		  		  		    	 		 		   		 		  
+Atlanta, Georgia 30332  		   	  			  	 		  		  		    	 		 		   		 		  
+All Rights Reserved  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+Template code for CS 4646/7646  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+Georgia Tech asserts copyright ownership of this template and all derivative  		   	  			  	 		  		  		    	 		 		   		 		  
+works, including solutions to the projects assigned in this course. Students  		   	  			  	 		  		  		    	 		 		   		 		  
+and other users of this template code are advised not to share it with others  		   	  			  	 		  		  		    	 		 		   		 		  
+or to make it available on publicly viewable websites including repositories  		   	  			  	 		  		  		    	 		 		   		 		  
+such as github and gitlab.  This copyright statement should not be removed  		   	  			  	 		  		  		    	 		 		   		 		  
+or edited.  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+We do grant permission to share solutions privately with non-students such  		   	  			  	 		  		  		    	 		 		   		 		  
+as potential employers. However, sharing with other current or future  		   	  			  	 		  		  		    	 		 		   		 		  
+students of CS 7646 is prohibited and subject to being investigated as a  		   	  			  	 		  		  		    	 		 		   		 		  
+GT honor code violation.  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+-----do not edit anything above this line---  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+Student Name: Tucker Balch (replace with your name)  		   	  			  	 		  		  		    	 		 		   		 		  
+GT User ID: jkok7 (replace with your User ID)  		   	  			  	 		  		  		    	 		 		   		 		  
+GT ID: 903550380 (replace with your GT ID)  		   	  			  	 		  		  		    	 		 		   		 		  
+"""  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+import numpy as np  		   	  			  	 		  		  		    	 		 		   		 		  
+import math  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+# this function should return a dataset (X and Y) that will work  		   	  			  	 		  		  		    	 		 		   		 		  
+# better for linear regression than decision trees  		   	  			  	 		  		  		    	 		 		   		 		  
+def best4LinReg(seed=1489683273): 
+    np.random.seed(seed)  		   	  			  	 		  		  		    	 		 		   		 		  
+    X = np.random.random(size = (100,8))*200-100 		   	  			  	 		  		  		    	 		 		   		 		  
+    # Here's is an example of creating a Y from randomly generated  		   	  			  	 		  		  		    	 		 		   		 		  
+    # X with multiple columns  		   	  			  	 		  		  		    	 		 		   		 		  
+    Y = X[:,0] + X[:,1] + X[:,2] + X[:,3]  + X[:,4] + X[:,5]  + X[:,6]  + X[:,7]  		   	  			  	 		  		  		    	 		 		   		 		  
+    return X, Y  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+def best4DT(seed=1489683273):  		   	  			  	 		  		  		    	 		 		   		 		  
+    np.random.seed(seed)  		   	  			  	 		  		  		    	 		 		   		 		  
+    X = np.random.random(size = (100, 8))*200-100
+    Y = np.random.random(size = (100, ))*200-100
+    X = np.tile(X, (9, 1))
+    Y = np.tile(Y, (9))
+    return X, Y  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+def author():  		   	  			  	 		  		  		    	 		 		   		 		  
+    return 'jkok7' #Change this to your user ID  		   	  			  	 		  		  		    	 		 		   		 		  
+  		   	  			  	 		  		  		    	 		 		   		 		  
+if __name__=="__main__":  		   	  			  	 		  		  		    	 		 		   		 		  
+    print("they call me Tim.")  		   	  			  	 		  		  		    	 		 		   		 		  
